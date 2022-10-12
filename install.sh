acabd9f5fb6f65617fbcf2d64988e8c19502ab65
Stanislau Arkhipenka <stanislau.arkhipenka@gmail.com>
2022-10-13 00:39:03 +0100

    Add systemd service and installation script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo cp -r $(pwd) /opt/sms_http_telegram
+python3 -m virtualenv -p python3 /opt/sms_http_telegram/.venv
+source /opt/sms_http_telegram/.venv/bin/activate
+pip3 install -r requirements.txt
+sed "s/MY_USER/$USER/g" example.service > sms_http_telegram.service
+sudo mv ./sms_http_telegram.service.service /etc/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl enable hexapod.service
